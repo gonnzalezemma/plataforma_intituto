76dfdcb7c4b213f2489630b6421979c6b043514d
gonnzalezemma <jorge_99.l@hotmail.com>
2022-10-02 20:41:49 -0300

    add controllers create and show profile user

--- controllers/perfilUsuarios.controllers.js
+++ controllers/perfilUsuarios.controllers.js
@@ -1,22 +1,52 @@
 const ctrlPerfilUsuarios={};
 
-
+const Usuario = require('../models/users')
 const Perfil =require('../models/perfiluser');
+const Materias =require('../models/materias');
 const bcryptjs = require('bcryptjs');
 const {generate_jwt}= require('../helpers/generate_jwt')
 
 
 //controlador para agregar datos
 ctrlPerfilUsuarios.agregarDatos = async(req, res)=>{
-    const {}=  req.body;
-// FALTA RELACION populate
-    const userId= req.usuario._id;
+    const {nombre,apellido,celular,direccion, carrera, materias}=  req.body;
 
-    const infoUser= new Perfil({})
+    const userId= req.usuario.id;
+
+    const usuario = await Usuario.findOne({_id:userId});
 
 
-await infoUser.save();
+
+
+    for (let i = 0; i < materias.length; i++) {
+        
+        const materia = materias[i];
+        
+
+        const objectMateria= await Materias.findOne({nombreMateria:materia})
+
+        var arrayCarreras = ([...arrayCarreras,
+            {
+            materias:objectMateria
+        } 
+    ])
+    }
+
+    arrayCarreras = ([...arrayCarreras,{
+        carrera:carrera
+    }])
+    
+
+
+    const infoUser= new Perfil({userId:usuario, nombre:nombre, apellido:apellido, celular:celular,direccion:direccion, carreras:arrayCarreras})
+
+
+    await infoUser.save();
+
+
 }
+//ejemplo
+
 
 //mostrar informacion de usuario
 ctrlPerfilUsuarios.rutaMostrarInformacion = async(req,res)=>{
