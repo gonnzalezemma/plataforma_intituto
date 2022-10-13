9e2d481ba3935db9f8938d01162e9c1c921fa030
gonnzalezemma <jorge_99.l@hotmail.com>
2022-10-13 10:28:53 -0300

    added controllers and routes

--- controllers/asistencia.constrollers.js
+++ controllers/asistencia.constrollers.js
@@ -1,49 +1,136 @@
-const ctrlAsistencia={};
-
-const Asistencia =require('../models/asistencia');
-const Usuario =require('../models/users')
+const ctrlAsistencia = {};
 
 
+const Asistencia = require("../models/asistencia");
+const Usuario = require("../models/users");
 
-ctrlAsistencia.tomarAsistencia = async(req, res)=>{
-
-    const busqueda={
-        asistencia,
-        dniAlumno
-    }=  req.body;
-    var datosAlumnos= [];
-    
-    //forOf  forin for each
-    for (let index = 0; index < busqueda.length; index++) {
-        const dniAlumnos = busqueda[index].dniAlumno;
-        const asistencias = busqueda[index].asistencia;
-        
-
-        const objectAlumno= await Usuario.findOne({dni:dniAlumnos})
-        datosAlumnos = ([...datosAlumnos,{
-            alumno:objectAlumno,
-            asistencia:asistencias
-        } ])
-    }
-    var currentDateObj = new Date();
-    var numberOfMlSeconds = currentDateObj.getTime();
-    var addMlSeconds = -(3*60) * 60000;
-    var newDateObj = new Date(numberOfMlSeconds + addMlSeconds);
-
-    const cargarAsistencia= new Asistencia({alumnos:datosAlumnos, fecha:newDateObj})
+ctrlAsistencia.tomarAsistencia = async (req, res) => {
 
 
- await cargarAsistencia.save();
 
 
+  const busqueda = { asistencia, dniAlumno } = req.body;
+  
+  const hoy = new Date().toISOString().slice(0, 10) 
  
+  var datosAlumnos = [];
 
-    res.json({msj: 'asistencia de alumnos es:',
-            obj:cargarAsistencia,
-            fecha:cargarAsistencia.fecha
-        })
+  const searchDate = await Asistencia.find({
+    fecha: {
+      $gt:new Date(hoy)
+    }
+});
 
 
+console.log(searchDate)
+
+if(searchDate.length = 0){
+
+    return res.status(409).json({msg:`Ya se a enviado la asistencia del dia de hoy`})
 }
+
+
+
+
+
+
+
+
+  for (let i = 0; i < busqueda.length; i++) {
+    const dniAlumnos = busqueda[i].dniAlumno;
+    const asistencias = busqueda[i].asistencia;
+
+    const objectAlumno = await Usuario.findOne({ dni: dniAlumnos });
+
+    datosAlumnos = [
+      ...datosAlumnos,
+      {
+        alumno: objectAlumno,
+        asistencia: asistencias,
+      },
+    ];
+  }
+  var currentDateObj = new Date();
+  var numberOfMlSeconds = currentDateObj.getTime();
+  var addMlSeconds = -(3 * 60) * 60000;
+  var newDateObj = new Date(numberOfMlSeconds + addMlSeconds);
+
+  const cargarAsistencia = new Asistencia({
+    alumnos: datosAlumnos,
+    fecha: newDateObj,
+  });
+
+  await cargarAsistencia.save();
+
+  res.json({
+    msj: "asistencia de alumnos es:",
+    obj: cargarAsistencia,
+    fecha: cargarAsistencia.fecha,
+  });
+};
+
+
+
+ctrlAsistencia.mostrarAsistencia = async (req, res) => {
+  const idAsistencia = req.params.id;
+
+  const hoy = new Date().toISOString().slice(0, 10)
+
+
+  console.log(hoy);
+  const asistencia = await Asistencia.find({
+    fecha: {
+      $gt:new Date(hoy)
+    }
+});
+
+  return res.status(200).json(asistencia);
+};
+
+
+
+ctrlAsistencia.mostrarAsistenciasAlumno = async (req, res) => {
+
+    const user = req.usuario;
+   
+    const asistencia = await Asistencia.find();
+    
+    
+
+
+    var asistenciasAlumno = [];
+    
+    asistencia.forEach(e => {
+        e.alumnos.forEach(a => {
+
+
+        if(a.alumno == user.id){
+
+
+              const asistencia = {
+                  alumno:a.alumno,
+                  asistencia:a.asistencia,
+              }
+
+              asistenciasAlumno = [...asistenciasAlumno, {asistenciaAlumno:asistencia} ]
+          
+          
+          } 
+
+      })
+
+        
+    });
+    const getAsistencia ={
+        asistencia: asistenciasAlumno,
+        clases: asistencia.length,
+        faltas: (asistencia.length - asistenciasAlumno.length)
+    }
+
+    return res.status(200).json(getAsistencia);
+  };
+  
+
+
 
 module.exports = ctrlAsistencia;
--- routes/asistencia.routes.js
+++ routes/asistencia.routes.js
@@ -5,7 +5,9 @@ const {validar_jwt} =require('../middlewares/validar_jwt')
 
 //controllers 
 const {
-    tomarAsistencia
+    tomarAsistencia,
+    mostrarAsistenciasAlumno,
+    mostrarAsistencia
 }= require('../controllers/asistencia.constrollers')
 
 
@@ -16,6 +18,10 @@ La asistencia tiene que ser tomada unicamente por el administrativo.
 
 */
 router.post('/asistencia',tomarAsistencia)
+
+router.get('/asistencia',validar_jwt,mostrarAsistenciasAlumno)
+
+router.get('/asistencia/date',mostrarAsistencia)
 
 
 
