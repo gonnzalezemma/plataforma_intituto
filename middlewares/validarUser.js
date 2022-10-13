0c919f8d5abc63f0ccaee7f549e87c3f0441d520
gonnzalezemma <jorge_99.l@hotmail.com>
2022-10-13 10:27:14 -0300

    added validation (tiene un error - a resolver-)

--- middlewares/validarUser.js
+++ middlewares/validarUser.js
@@ -1,21 +1,60 @@
 const { body, check } = require('express-validator');
 const { ExisteEmail } = require('../middlewares/validar_email');
+const {validar_campos} = require('../helpers/validar_campos');
 const validarUser=[
 
 
-    body('email', 'el correo ingresado no contiene un formato correcto')
-        .isEmail()
-        .not()
-        .isEmpty()
-        .custom(ExisteEmail),
-    
-     body('password', 'El password ingresado no contiene un formato correcto')
+    body('dni', 'El password ingresado no contiene un formato correcto')
+   .isString()
+   .not()
+   .isEmpty()
+   .isLength({ min: 3, max: 8}),
+   
+    body('tipoRole', 'El tipo ingresado no contiene un formato correcto')
+   .isString()
+   .not()
+   .isEmpty()
+   .isLength({ min: 1, max: 8}),
+    validar_campos
+]
+
+const validarInformacion =[,
+    body('nombre', 'El nombre ingresado contiene un formato incorrecto')
     .isString()
     .not()
     .isEmpty()
+    ,
+    body('apellido',' El apellido ingresado no contiene un formato correcto')
+    .isString()
+    .not()
+    .isEmpty()
+    ,
+    body('celular',' El celular ingresado no contiene un formato correcto')
+    .isString()
+    .not()
+    .isEmpty()
+    ,
+    
+body('email', 'el correo ingresado no contiene un formato correcto')
+    .isEmail()
+    .not()
+    .isEmpty()
+    .custom(ExisteEmail)
+    ,
+    
+    body('direccion','La direccion ingresado no contiene un formato correcto')
+    .isString()
+    .not()
+    .isEmpty()
+    ,
+    
+    body('carrera','La carrera ingresado no contiene un formato correcto')
+    .isString()
+    .not()
+    .isEmpty()
+    ,
 
 ]
-
 module.exports = {
     validarUser
 }
